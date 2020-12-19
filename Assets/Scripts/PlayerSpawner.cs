aa7d009a0997a6df8e381912250418a9322f1d19
falconstrike209 <75436231+falconstrike209@users.noreply.github.com>
2020-12-19 10:35:46 -0500

    Some changes
    not a lot here, just changed the import settings and added that spawner as shown in the clip I sent you, and removd the slippery material from the project altogether.

---
+++ Assets/Scripts/PlayerSpawner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawner : MonoBehaviour
+{
+
+    public GameObject p1Prefab;
+    public GameObject DestroyOnSpawn;
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.X))
+        {
+            GameObject a = Instantiate(p1Prefab, transform.position, transform.rotation) as GameObject;
+            Destroy(DestroyOnSpawn);
+        }
+    }
+}
