50a082ef185450b948fc2d022619317111e0464b
falconstrike209 <75436231+falconstrike209@users.noreply.github.com>
2021-01-23 11:58:20 -0500

    Player movement, camera, and parallax
    need finalized camera script, hitbox error fixed, or just ignore as we're switching to sprite based. (probably) and system for multiplayer inputs.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,67 +2,79 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
-    public Rigidbody2D rb;
-    public float movementSpeed;
+    [SerializeField] private Rigidbody2D rb2d;
+    [SerializeField] private float _xAxisDrag = 0.05f;
+    private float _horizontalSpeed = 0;
+    public float speed = 2f;
     public float jumpForce;
     public Transform groundDetector;
     public LayerMask groundLayers;
     public bool facingRight;
+    public bool IsGrounded;
 
     public Animator animator;
 
-    float mx;
-
     private void Update()
     {
-        mx = Input.GetAxisRaw("Horizontal");
+
+        Collider2D groundCheck = Physics2D.OverlapCircle(groundDetector.position, 0.1f, groundLayers);
+
+        if (groundCheck != null)
+        {
+            IsGrounded = true;
+            animator.SetBool("Jump", false);
+        }
+        else if (groundCheck == null)
+        {
+            IsGrounded = false;
+            animator.SetBool("Jump", true);
+        }
+
+        if (IsGrounded == true)
+        {
+            animator.SetBool("Jump", false);
+        }
+
+        _horizontalSpeed = Input.GetAxisRaw("Horizontal");
 
         Flip();
 
-        if (Input.GetButtonDown("Jump") && IsGrounded())
+        if (IsGrounded == true && Input.GetButtonDown("Jump"))
         {
-            animator.SetBool("Jump", true);
             Jump();
         }
 
-        animator.SetFloat("yVelocity", rb.velocity.y);
+
+        animator.SetFloat("yVelocity", rb2d.velocity.y);
 
     }
 
     private void FixedUpdate()
     {
-        animator.SetFloat("Speed", Mathf.Abs(mx));
 
-        Vector2 movement = new Vector2(mx * movementSpeed, rb.velocity.y);
+        animator.SetFloat("Speed", Mathf.Abs(_horizontalSpeed));
 
-        rb.velocity = movement;
+        rb2d.AddForce(new Vector2(_horizontalSpeed * speed, 0), ForceMode2D.Impulse);
+
+        Vector3 velocity = rb2d.velocity;
+        velocity.x = velocity.x / (_xAxisDrag + 1f);
+        rb2d.velocity = velocity;
 
     }
 
     void Jump()
     {
-        Vector2 movement = new Vector2(rb.velocity.x, jumpForce);
+        Vector2 movement = new Vector2(rb2d.velocity.x, jumpForce);
 
-        rb.velocity = movement;
-    }
-
-    public bool IsGrounded()
-    {
-        Collider2D groundCheck = Physics2D.OverlapCircle(groundDetector.position, 0.1f, groundLayers);
-
-        if (groundCheck != null)
-        {
-            return true;
-        }
-
-        return false;
+        rb2d.velocity = movement;
     }
 
     void Flip()
     {
-        if ((mx < 0 && facingRight) || (mx > 0 && !facingRight))
+        if ((_horizontalSpeed < 0 && facingRight) || (_horizontalSpeed > 0 && !facingRight))
         {
             facingRight = !facingRight;
             transform.Rotate(new Vector3(0, 180, 0));
